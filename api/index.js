36908e5942b4704dcb5337439a93538ba1854cb0
vigofirmansyah <vigofirmansyahputra@gmail.com>
2024-05-02 17:50:42 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const connect = process.env.MONGO;
+mongoose
+  .connect(connect)
+  .then((err) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 const port = 3000;
 app.listen(port, () => {
