80b844fa37b72e9601f0dde4d407b0fd436ce4f2
vigofirmansyah <vigofirmansyahputra@gmail.com>
2024-05-03 15:41:54 +0700

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 const connect = process.env.MONGO;
 mongoose
@@ -14,8 +15,10 @@ mongoose
   });
 const app = express();
 const port = 3000;
+app.use(express.json());
 app.listen(port, () => {
   console.log("service is running on port 3000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
